aaae92419aa5e9b9168f690e68ee4d56203c61c8
Anaz Anoiar <98679895+AnazAnoiar69@users.noreply.github.com>
2023-03-15 00:57:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-const port = 80;
+const port = 8000;
 
 app.use(express.static('public'));
 
