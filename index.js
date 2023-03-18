2801d86ccde565edec3e190948443c7c850fd04a
Anaz Anoiar <98679895+AnazAnoiar69@users.noreply.github.com>
2023-03-18 11:46:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,15 @@
 const express = require('express');
 const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
 
-const port = 8000;
-
-app.use(express.static('public'));
-
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+app.post('/api', (req, res) => {
+  console.log(req.body); // handle incoming data
+  res.sendStatus(200); // send a response
 });
 
-io.on('connection', function(socket) {
-  console.log('A user connected');
-
-  socket.on('disconnect', function() {
-    console.log('A user disconnected');
-  });
-
-  socket.on('data', function(data) {
-    console.log('Data received:', data);
-    io.emit('data', data);
-  });
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
 });
 
-http.listen(port, function(e) {
-  console.log('Listening on port', port);
-  console.log(e);
+const server = app.listen(3000, () => {
+  console.log(`Server listening on port ${server.address().port}`);
 });
